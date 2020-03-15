270616e6da522863f859cd923e4d091994514d18
jackli562770 <562770@gmail.com>
2020-03-15 22:03:41 +0800

    java & kotlin  variable,data type

--- src/main/java/com/test/Hello.java
+++ src/main/java/com/test/Hello.java
@@ -5,5 +5,16 @@ public class Hello {
 //       System.out.println("Hello world!");
         Person p = new Person();
         p.hello();
+        int age = 19;
+        Integer age2 = 19;
+        char c = 't';
+        Character c2 = 't';
+        byte b = 120;
+        Byte b2 = 120;
+        float weight = 66.5f;
+        float height = 1.7f;
+        boolean adult = true;
+        String name = "test";
+
     }
 }
--- src/main/kotlin/com/test/Hello.kt
+++ src/main/kotlin/com/test/Hello.kt
@@ -6,6 +6,11 @@ fun main() {
     //Human().hello();
     val h = Human();
     h.hello();
+    val age : Int = 19;
+    var weight = 60.5f;
+    var name:String; //若未先給值時，kotlin需要先給變數型態，否則因為無法自動推斷會出錯
+    //alt + 9  git資訊
+    //alt + `  commit file
 }
 
 class Human {
